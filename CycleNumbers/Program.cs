fe1994a89e118ed42ee94e174cf694d804cf18e6
DreaMeR <DreaMeR@DREAMER>
2024-07-12 11:26:30 +0600

    Добавьте файлы проекта.

---
+++ CycleNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CycleNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxValue = 999;
+            int stepForNumbers = 7;
+
+            for (int i = 5; i < maxValue; i += stepForNumbers)
+            {
+                Console.WriteLine(i);
+                if(i >= 103)
+                    break;
+            }
+        }
+    }
+}
